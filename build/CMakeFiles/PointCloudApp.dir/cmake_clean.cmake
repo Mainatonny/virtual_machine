c9e706b707cbac82c1afcbd56fc0799ded41c627
Mainatonny <123733695+Mainatonny@users.noreply.github.com>
2025-01-10 04:35:15 +0000

    Added images to project

---
+++ build/CMakeFiles/PointCloudApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointCloudApp.dir/main.cpp.o"
+  "PointCloudApp"
+  "PointCloudApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointCloudApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
